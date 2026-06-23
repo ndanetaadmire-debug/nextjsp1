2c94121db61e17972ca7b4f6d1ebb03871e86a30
ndanetaadmire-debug <ndanetaadmire@gmail.com>
2026-06-23 14:24:36 +0200

    design: centered profile image using wrapper div

--- components/layout.js
+++ components/layout.js
@@ -1,40 +1,52 @@
 import styles from "./layout.module.css";
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image"
-import utilsStyles from "../styles/utils.module.css"
+import Image from "next/image";
+import utilsStyles from "../styles/utils.module.css";
 
-export default function Layout({children, home}){
-    return <div className={styles.container}>
-        <Head>
-            <meta name = "description" content = "profile and blog details" />
-            <meta name ="profile" content = "profile"/>
-            <title> Next.js Blog</title>
-             
-</Head>
-<header>
-    {home ? (<Image className = {utilsStyles.boarderRadius}
-    priority
-    src ="/profile pic.jpeg"
-    alt = "face" width ={180} height ={180} />)  : 
-    (<Link href="/">
-       <Image className = {utilsStyles.boarderRadius}
-        priority
-    src ="/profile pic.jpeg"
-    alt = "face"
-        width = {100}
-        height = {100} />  </Link>) }
-</header>
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta name="description" content="profile and blog details" />
+        <meta name="profile" content="profile" />
+        <title> Next.js Blog</title>
+      </Head>
+      <header>
+        {home ? 
+          <div className={utilsStyles.imageWrapper}>
+            
+            <Image
+              className={utilsStyles.boarderRadius}
+              priority
+              src="/profile pic.jpeg"
+              alt="face"
+              width={180}
+              height={180}
+            />
+            
+          </div>
+         : (
+          <Link href="/">
+            <Image
+              className={utilsStyles.boarderRadius}
+              priority
+              src="/profile pic.jpeg"
+              alt="face"
+              width={100}
+              height={100}
+            />
+          </Link>
+        )}
+      </header>
 
-<main>{children}</main>
+      <main>{children}</main>
 
-    {!home && (<Link href="/">
-    <div className = {utilsStyles.backHome}> 
-        Back to home
-        </div>
-    </Link>)}
-
-
-
-</div>
+      {!home && (
+        <Link href="/">
+          <div className={utilsStyles.backHome}>Back to home</div>
+        </Link>
+      )}
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,52 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
 import utilsStyles from "../styles/utils.module.css";
-import {getSortedPostsData} from "../lib/posts";
+import { getSortedPostsData } from "../lib/posts";
 import Date from ".././components/date";
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
-    props: {allPostsData}
+    props: { allPostsData },
   };
-};
+}
 
-
-
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-    < Head> <title>My blog</title>
+      <Head>
+        {" "}
+        <title>My blog</title>
       </Head>
-      <header> <h1> Hello</h1></header>
+      <header>
+        {" "}
+        <h1> Hello</h1>
+      </header>
 
       <p className={utilsStyles.texting}>I love my wife so much</p>
       <p>I love you Lord Jesus</p>
 
-      <ul className ={utilsStyles.list}>
+      <ul className={utilsStyles.list}>
         <li>
           <Link href="/posts/first-post">Read my first post</Link>
         </li>
       </ul>
 
-<section className={utilsStyles.heading}>
-<h2 className={utilsStyles.headingLg}>
-Blog
-</h2>
-<ul className={utilsStyles.list}>{allPostsData.map(({id,title,date})=>(
-<li className={utilsStyles.listItem} key={id}> <Link href={`../posts/${id}`}>
-{title}</Link><br/><Date dateString={date} />
-</li>)
-)}
-
-
-
-</ul>
-
-</section>
-
-
-
-
+      <section className={utilsStyles.heading}>
+        <h2 className={utilsStyles.headingLg}>Blog</h2>
+        <ul className={utilsStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={utilsStyles.listItem} key={id}>
+              {" "}
+              <Link href={`../posts/${id}`}>{title}</Link>
+              <br />
+              <Date dateString={date} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  )};
+  );
+}
